4cf89e38a41b35956b8b0a1022ad70d05e2eeaff
Mark Mullan <markrmullan@gmail.com>
2019-01-21 12:28:06 -0800

    re-configure TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TopAppBar, { TopAppBarFixedAdjust } from '@material/react-top-app-bar';
+import MaterialIcon from '@material/react-material-icon';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <TopAppBar
+          title='Miami, FL'
+          navigationIcon={<MaterialIcon
+            icon='menu'
+            onClick={() => console.log('click')}
+          />}
+          actionItems={[<MaterialIcon key='item' icon='bookmark' />]}
+        />
+        <TopAppBarFixedAdjust>
+          My exciting content!
+        </TopAppBarFixedAdjust>
+      </>
     );
   }
 }
