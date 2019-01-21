21eae98554883be15629fe2a76d631d753e35623
Mark Mullan <markrmullan@gmail.com>
2019-01-21 12:49:35 -0800

    set initial value of locale based on browser locale

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,9 @@
 import i18n from "i18next";
+import detector from "i18next-browser-languagedetector";
 import { reactI18nextModule } from "react-i18next";
 
 i18n
+  .use(detector)
   .use(reactI18nextModule) // passes i18n down to react-i18next
   .init({
     resources: {
