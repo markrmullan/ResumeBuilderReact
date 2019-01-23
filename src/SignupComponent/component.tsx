51a66dc9cdef7c6962afb02922bf51b99c247bc8
Mark Mullan <markrmullan@gmail.com>
2019-01-22 22:32:23 -0800

    some signup form fields, and an async button that doesn't yet make an actual request

--- src/SignupComponent/component.tsx
+++ src/SignupComponent/component.tsx
@@ -1,11 +1,84 @@
 import React, { Component } from 'react';
 import { withNamespaces, WithNamespaces } from 'react-i18next';
+import AsyncButton from 'react-async-button';
+import TextField, { Input } from '@material/react-text-field';
 
-class Signup extends Component<WithNamespaces> {
+import './styles.scss';
+
+type SignupState = {
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+};
+
+class Signup extends Component<WithNamespaces, SignupState> {
+  public state = {
+    email: '',
+    password: '',
+    passwordConfirmation: ''
+  };
+
   render() {
+    const { t } = this.props;
+    const { email, password, passwordConfirmation } = this.state;
+
     return (
-      <div>Signup form goes here</div>
+      <form>
+        <TextField
+          label={t('email')}
+        >
+          <Input
+            name='email'
+            value={email}
+            onChange={(e) => this.onChange(e)}/>
+        </TextField>
+
+        <TextField
+          label={t('create_a_password')}
+        >
+          <Input
+            name='password'
+            type='password'
+            autoComplete='new-password'
+            value={password}
+            onChange={(e) => this.onChange(e)}/>
+        </TextField>
+
+        <TextField
+          label={t('confirm_password')}
+        >
+          <Input
+            name='passwordConfirmation'
+            type='password'
+            autoComplete='new-password'
+            value={passwordConfirmation}
+            onChange={(e) => this.onChange(e)}/>
+        </TextField>
+
+        <AsyncButton
+          className="mdc-button mdc-ripple-upgraded mdc-button--raised"
+          raised={true}
+          text={t('create_account')}
+          pendingText={t('pending_ellipsis')}
+          onClick={this.submit}
+         />
+      </form>
     )
+  }
+
+  onChange(e: React.FormEvent<HTMLTextAreaElement>) {
+    const { name, value } = e.currentTarget;
+
+    this.setState({ [name]: value } as SignupState);
+  }
+
+  submit(_: React.FormEvent<HTMLButtonElement>) {
+    return new Promise((resolve, _) => {
+      let wait = setTimeout(() => {
+        clearTimeout(wait);
+        resolve('Promise A win!');
+      }, 2000)
+    })
   }
 }
 
