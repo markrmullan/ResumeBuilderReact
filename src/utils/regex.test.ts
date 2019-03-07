d151f94a66f2f95ecc2a61a3fd8939622321c746
Mark Mullan <markrmullan@gmail.com>
2019-03-07 14:46:23 -0800

    configure Jest to run in verbose mode. add some additional regexes and regex tests

--- src/utils/regex.test.ts
+++ src/utils/regex.test.ts
@@ -1,6 +1,59 @@
-import { EMAIL_REQUIRED } from 'utils/regex';
+import {
+  ALPHANUMERIC_OPTIONAL,
+  ANYTHING,
+  AT_LEAST_ONE_CHARACTER,
+  EMAIL,
+  EMAIL_REQUIRED
+} from 'utils/regex';
 
 describe('regexes', () => {
+  it('ANYTHING', () => {
+    testRegex(ANYTHING, [
+      '',
+      'a',
+      'valid-email@gmail.com',
+      '8',
+      'diácritïcs okay',
+      'punctuation okay.'
+    ]);
+  });
+
+  it('ALPHANUMERIC_OPTIONAL', () => {
+    testRegex(ALPHANUMERIC_OPTIONAL, [
+      '',
+      '9',
+      'ABC',
+      'R2D2'
+    ], [
+      'diácritics not okay',
+      'puncuation not-okay'
+    ]);
+  });
+
+  it('AT_LEAST_ONE_CHARACTER', () => {
+    testRegex(AT_LEAST_ONE_CHARACTER, [
+      'a',
+      'abc',
+      'spaces okay',
+      '8',
+      'punctuation okay.',
+      '...leading punctuation also okay'
+    ], [
+      ''
+    ]);
+  });
+
+  it('EMAIL', () => {
+    testRegex(EMAIL, [
+      '',
+      'team@resumebuilder.io',
+      'support@google.com'
+    ], [
+      'teamresumebuilder.io',
+      'test'
+    ]);
+  });
+
   it('EMAIL_REQUIRED', () => {
     testRegex(EMAIL_REQUIRED, [
       'team@resumebuilder.io',
--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,1 +1,7 @@
+export const ANYTHING: RegExp = /^.*$/;
+export const ALPHANUMERIC_OPTIONAL: RegExp = /^[a-zA-Z\d]*$/;
+
+export const AT_LEAST_ONE_CHARACTER: RegExp = /^.+$/;
+
+export const EMAIL: RegExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|^$/;
 export const EMAIL_REQUIRED: RegExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
