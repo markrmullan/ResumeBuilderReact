402e3a619e00957b0a360aa0412b70aa4c55a4a6
Mark Mullan <markrmullan@gmail.com>
2020-04-13 19:57:01 -0700

    fix redirect to login page on HTTP 401 res

--- src/middleware/axios.ts
+++ src/middleware/axios.ts
@@ -12,10 +12,10 @@ axios.createWith401Handler = (config: AxiosRequestConfig) => {
   const axiosInstance = axios.create(config);
 
   axiosInstance.interceptors.response.use(_ => _, error => {
-    let { href } = window.location;
+    const { pathname } = window.location;
 
-    if (error.response.status === 401 && href !== ROUTES.login) {
-        href = ROUTES.login;
+    if (error.response.status === 401 && pathname !== ROUTES.login) {
+        window.location.href = ROUTES.login;
       }
 
     return Promise.reject(error);
