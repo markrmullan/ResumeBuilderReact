68d21fdd3e908a4d026d3c407723c7543a550287
Mark Mullan <markrmullan@gmail.com>
2020-04-25 22:08:26 -0700

    add some spacing, tweak some copy

--- src/EditResume/index.tsx
+++ src/EditResume/index.tsx
@@ -58,7 +58,7 @@ class EditResumeComponent extends Component<TComponentProps, TComponentState> {
           <ArrowBackRounded fontSize="large" style={{ color: deepPurple[500] }} />
         </Link>
         <Row>
-          <Col xs={12} md={{ span: 10, offset: 1 }} lg={{ span: 7, offset: 0 }} xl={6}>
+          <Col xs={12} md={{ span: 10, offset: 1 }} lg={{ span: 7, offset: 0 }} xl={6} className={styles.outerResumeCol}>
             <Container fluid className={styles.outerContainer}>
               <Row className={`${styles.resumeNameContainer} ${styles.mb16}`}>
                 <Col xs={8} sm={5} md={4} xl={3}>
