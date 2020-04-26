18c7d03b7417a40caeb86fcf013b850a8fc0d9c9
Mark Mullan <markrmullan@gmail.com>
2020-04-25 19:27:08 -0700

    fix resume card layout on mobile

--- src/DashboardComponent/ResumeCard/index.tsx
+++ src/DashboardComponent/ResumeCard/index.tsx
@@ -43,7 +43,7 @@ class ResumeCardComponent extends PureComponent<TComponentProps, TComponentState
       <Card className={styles.container}>
         <Container fluid>
           <Row>
-            <Col xs={{ span: 12, offset: 7 }} sm={{ span: 12, offset: 8 }} md={{ span: 12, offset: 6 }}>
+            <Col xs={{ span: 12, offset: 4 }} sm={{ span: 12, offset: 8 }} md={{ span: 12, offset: 6 }}>
               <div className={styles.infoContainer}>
                 <Typography className={styles.name}>
                   {resume.name}
