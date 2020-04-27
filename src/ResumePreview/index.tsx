a921507101d9db26cd95288f06e7e8de7f55bafd
Mark Mullan <markrmullan@gmail.com>
2020-04-27 16:45:40 -0700

    reorder link URL regex to properly strip 'https://www.' from the prefix of a url

--- src/ResumePreview/index.tsx
+++ src/ResumePreview/index.tsx
@@ -179,7 +179,7 @@ class ResumePreviewComponent extends PureComponent<TComponentProps> {
     const { links = [] } = this.props.resume;
 
     return links.reduce((acc, link) => {
-      const urlWithoutProtocol = link.url && link.url.replace(/^(https:\/\/|http:\/\/|https:\/\/www\.|http:\/\/www\.)/, '');
+      const urlWithoutProtocol = link.url && link.url.replace(/^(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)/, '');
 
       if (urlWithoutProtocol) {
         acc.push(
