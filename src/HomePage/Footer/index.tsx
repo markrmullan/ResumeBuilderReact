9a1a41d9595b83cc65bd36b3a3e5bb2a918d4180
Mark Mullan <markrmullan@gmail.com>
2020-04-30 14:18:42 -0700

    add social links

--- src/HomePage/Footer/index.tsx
+++ src/HomePage/Footer/index.tsx
@@ -4,6 +4,8 @@ import { Typography } from '@material-ui/core';
 import { Col, Container, Row } from 'react-bootstrap';
 import { WithNamespaces, withNamespaces } from 'react-i18next';
 
+import facebookIcon from './facebook-32.png';
+import linkedinIcon from './linkedin-32.png';
 import styles from './styles.module.scss';
 
 type TComponentProps = WithNamespaces;
@@ -19,6 +21,26 @@ class FooterComponent extends PureComponent<TComponentProps> {
             <Typography align="center" variant="subtitle1" className={styles.madeWith}>
               {t('home_page.made_with_muscle')}
             </Typography>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col className={styles.socialContainer}>
+            <a
+              href="https://www.linkedin.com/company/easy-resume-co"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img className={styles.socialMediaIcon} src={linkedinIcon} alt={t('home_page.linkedin')} />
+            </a>
+
+            <a
+              href="https://fb.me/easyresumeco"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img className={styles.socialMediaIcon} src={facebookIcon} alt={t('home_page.facebook')} />
+            </a>
           </Col>
         </Row>
       </Container>
