a6e3141b32cde6086dc67c51b8a2d7ca2d2b971a
Mark Mullan <markrmullan@gmail.com>
2020-05-03 15:19:27 -0700

    email regex fix

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -3,5 +3,5 @@ export const ALPHANUMERIC_OPTIONAL: RegExp = /^[a-zA-Z\d]*$/;
 
 export const AT_LEAST_ONE_CHARACTER: RegExp = /^.+$/;
 
-export const EMAIL: RegExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|^$/;
-export const EMAIL_REQUIRED: RegExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+export const EMAIL: RegExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|^$/i;
+export const EMAIL_REQUIRED: RegExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i;
